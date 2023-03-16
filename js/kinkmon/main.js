575e59061f1ea031ef0292641d9fc6b5d8bf3442
flopsable <chrand14@gmail.com>
2023-03-16 22:51:00 +1300

    https://github.com/flopsable/flopsable.github.io/issues/1 First Load of Images

--- js/kinkmon/main.js
+++ js/kinkmon/main.js
@@ -3,13 +3,35 @@ var pageInitFunctions = [];
 pageInitFunctions.push(function() {
     initKinks();
 });
-
+/*
+<div class="col">
+            Column
+          </div>
+          <div class="col">
+            Column
+          </div>
+          <div class="col">
+            Column
+          </div>
+*/
 function initKinks() {
-    alert("Document Ready");
+    var json = $.ajax({
+        url: "/json/images.json",
+        type: "GET"
+    }).done(function(resp) {
+        var kinkboard = $(".kinkboard").find("div.row");
+        for (var index in resp.kinkmon) {
+            var type = (resp.kinkmon[index]);
+            var col = $(<div></div>).addClass("col");
+            var image = $(<img></img>).attr("src", "/images/kinkmon/" + type.file);
+            col.append(image);
+            kinkboard.append(col);
+        }
+    })
 }
 
 $(document).ready(function(e) {
-    for (func in pageInitFunctions) {
-        func();
+    for (index in pageInitFunctions) {
+        pageInitFunctions[index]();
     }
 });
